481c177911fdca9efa3521df8231e4ae19a0b199
roman.vasilev <>
2018-08-29 01:13:55 +0400

    fix: Update source bug when using eslint in watch mode

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,7 @@
+import * as ts from 'typescript';
 import * as assert from 'assert';
 import * as lib from './index';
+import mockFs = require('mock-fs');
 
 const root = process.cwd();
 
@@ -28,13 +30,13 @@ describe('tsconfig-files', () => {
 
     it('get source file which are not in files', () => {
         const testFile = `${root}/test-project/file.spec.ts`;
-        const sourceFile = service.getSourceFile(testFile);
+        const sourceFile = service.getSourceFile(testFile, undefined);
         assert(sourceFile);
     });
 
     it('typescript checker (file which is not defined in tsconfig)', () => {
         const testFile = `${root}/test-project/file.spec.ts`;
-        const sourceFile = service.getSourceFile(testFile);
+        const sourceFile = service.getSourceFile(testFile, undefined);
         const checker = service.getProgram().getTypeChecker();
         const [itstmt] = sourceFile.statements.filter(x => x.getText() === `it('example test');`);
         const itid = (itstmt as any).expression.expression;
@@ -58,7 +60,7 @@ describe('create service', () => {
         const testFile = `${root}/test-project/errors.ts`;
         const sourceFile = service.getProgram().getSourceFile(testFile);
         assert(sourceFile);
-        const diagnostics = service.getDiagnostics(testFile);
+        const diagnostics = service.getDiagnostics(testFile, undefined);
         assert.equal(diagnostics.length, 2);
         assert.equal(diagnostics[0].messageText, `Type '1' is not assignable to type 'string'.`);
         assert.equal(diagnostics[1].messageText, `Type '"foo"' is not assignable to type 'number'.`);
@@ -68,7 +70,7 @@ describe('create service', () => {
         const testFile = `${root}/test-project/number.ts`;
         const sourceFile = service.getProgram().getSourceFile(testFile);
         assert(sourceFile);
-        const diagnostics = service.getDiagnostics(testFile);
+        const diagnostics = service.getDiagnostics(testFile, undefined);
         assert.equal(diagnostics.length, 0);
     });
 
@@ -76,7 +78,7 @@ describe('create service', () => {
         const testFile = `${root}/test-project/builtin.ts`;
         const sourceFile = service.getProgram().getSourceFile(testFile);
         assert(sourceFile);
-        const diagnostics = service.getDiagnostics(testFile);
+        const diagnostics = service.getDiagnostics(testFile, undefined);
         assert.equal(diagnostics.length, 0);
     });
 
@@ -84,7 +86,7 @@ describe('create service', () => {
         const testFile = `${root}/test-project/types.ts`;
         const sourceFile = service.getProgram().getSourceFile(testFile);
         assert(sourceFile);
-        const diagnostics = service.getDiagnostics(testFile);
+        const diagnostics = service.getDiagnostics(testFile, undefined);
         assert.equal(diagnostics.length, 0);
     });
 
@@ -92,7 +94,7 @@ describe('create service', () => {
         const testFile = `${root}/test-project/decorator.ts`;
         const sourceFile = service.getProgram().getSourceFile(testFile);
         assert(sourceFile);
-        const diagnostics = service.getDiagnostics(testFile);
+        const diagnostics = service.getDiagnostics(testFile, undefined);
         assert.equal(diagnostics.length, 0);
     });
 
@@ -100,7 +102,7 @@ describe('create service', () => {
         const testFile = `${root}/test-project/global-types.ts`;
         const sourceFile = service.getProgram().getSourceFile(testFile);
         assert(sourceFile);
-        const diagnostics = service.getDiagnostics(testFile);
+        const diagnostics = service.getDiagnostics(testFile, undefined);
         assert.equal(diagnostics.length, 0);
     });
 
@@ -108,8 +110,41 @@ describe('create service', () => {
         const testFile = `${root}/test-project/date.ts`;
         const sourceFile = service.getProgram().getSourceFile(testFile);
         assert(sourceFile);
-        const diagnostics = service.getDiagnostics(testFile);
+        const diagnostics = service.getDiagnostics(testFile, undefined);
         assert.equal(diagnostics.length, 0);
+    });
+
+});
+
+describe('source file of changed file', () => {
+
+    const testFile = `${root}/test-project/amok.ts`;
+    const configFile = `${root}/test-project/tsconfig-empty.json`;
+    let sourceFile: ts.SourceFile;
+
+    before(() => {
+        mockFs({
+            [testFile]: 'const x = 1',
+            [configFile]: '{}',
+        });
+        service = lib.createService({ configFile });
+        sourceFile = service.getProgram().getSourceFile(testFile);
+    });
+
+    after(() => {
+        mockFs.restore();
+    });
+
+    it('smoke', () => {
+        assert(service);
+        assert(sourceFile);
+    });
+
+    it('changes should be reflected', () => {
+        mockFs({ [testFile]: 'let x = 2' });
+        sourceFile = service.getSourceFile(testFile, 'let x = 2');
+        assert(sourceFile);
+        assert.equal(sourceFile.getText(), 'let x = 2');
     });
 
 });
